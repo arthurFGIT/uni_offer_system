695c665ab5c64e40d55c192c0a1496160c6fc93a
Arthur Fieguth <arthur.fieguth@student.hs-rm.de>
2022-05-03 18:38:13 +0200

    fixing empty String list

--- src/main/java/de/hsrm/mi/web/projekt/benutzerprofil/BenutzerProfil.java
+++ src/main/java/de/hsrm/mi/web/projekt/benutzerprofil/BenutzerProfil.java
@@ -2,6 +2,7 @@ package de.hsrm.mi.web.projekt.benutzerprofil;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.format.annotation.DateTimeFormat;
@@ -26,11 +27,7 @@ public class BenutzerProfil{
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ((adresse == null) ? 0 : adresse.hashCode());
-        result = prime * result + ((email == null) ? 0 : email.hashCode());
         result = prime * result + ((geburtsdatum == null) ? 0 : geburtsdatum.hashCode());
-        result = prime * result + ((interessen == null) ? 0 : interessen.hashCode());
-        result = prime * result + ((lieblingsfarbe == null) ? 0 : lieblingsfarbe.hashCode());
         result = prime * result + ((name == null) ? 0 : name.hashCode());
         return result;
     }
@@ -43,30 +40,10 @@ public class BenutzerProfil{
         if (getClass() != obj.getClass())
             return false;
         BenutzerProfil other = (BenutzerProfil) obj;
-        if (adresse == null) {
-            if (other.adresse != null)
-                return false;
-        } else if (!adresse.equals(other.adresse))
-            return false;
-        if (email == null) {
-            if (other.email != null)
-                return false;
-        } else if (!email.equals(other.email))
-            return false;
         if (geburtsdatum == null) {
             if (other.geburtsdatum != null)
                 return false;
         } else if (!geburtsdatum.equals(other.geburtsdatum))
-            return false;
-        if (interessen == null) {
-            if (other.interessen != null)
-                return false;
-        } else if (!interessen.equals(other.interessen))
-            return false;
-        if (lieblingsfarbe == null) {
-            if (other.lieblingsfarbe != null)
-                return false;
-        } else if (!lieblingsfarbe.equals(other.lieblingsfarbe))
             return false;
         if (name == null) {
             if (other.name != null)
@@ -113,10 +90,13 @@ public class BenutzerProfil{
     }
 
     public List<String> getInteressenListe(){
+        if(interessen.isEmpty()){
+            return Collections.emptyList();
+        }
         List<String> interessenListe = new ArrayList<String>();
         for(String x : interessen.split(",")){
             interessenListe.add(x.trim());
-        }        
+        }   
         return interessenListe;
     }
 
